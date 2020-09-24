f35781db592ee7c265d8a6e83a52d5e01c02d2b8
jackLiu <54433489@qq.com>
2020-09-24 23:27:55 +0800

    try to pass the test cases

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into t_coffee (name, price, create_time, update_time) values ('espresso', 21.70, now(), now());
-insert into t_coffee (name, price, create_time, update_time) values ('latte', 25.00, now(), now());
+insert into t_coffee (name, price, create_time, update_time) values ('latte', 25.20, now(), now());
 insert into t_coffee (name, price, create_time, update_time) values ('capuccino', 25.00, now(), now());
 insert into t_coffee (name, price, create_time, update_time) values ('mocha', 33.80, now(), now());
-insert into t_coffee (name, price, create_time, update_time) values ('macchiato', 30.00, now(), now());
+insert into t_coffee (name, price, create_time, update_time) values ('macchiato', 30.60, now(), now());
